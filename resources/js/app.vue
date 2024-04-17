7676047417682b55eaf83fa022e3a327528dd44e
Superluis3629 <108126524+Superluis3629@users.noreply.github.com>
2024-04-17 11:17:30 +0700

    frist commit project v1-02

---
+++ resources/js/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hiiiiiiii</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MiniPOS8</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Hello, MiniPOS8 V11</h1>
+    <hr>
+    <div id="root">
+
+    </div>
+
+    @vite('resources/js/app.js')
+</body>
+</html>
